b0506e58a4cb17e9b72ac34c253e0983cc99c2da
Colin Hayes <colin.hayes985@gmail.com>
2018-07-09 17:30:26 -0400

    Create MessageForm component and add to page

---
+++ src/MessageForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class MessageForm extends Component{
+    render(){
+        return(
+            <form className="MessageForm">
+                <input type="text" name="body" placeholder="Type Message Here..." autoFocus required/>
+                <button type="submit">
+                    Send
+                </button>
+            </form>
+        )
+    }
+}
+
+export default MessageForm
