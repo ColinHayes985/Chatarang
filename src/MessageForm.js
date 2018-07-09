1385a57d161c7f89c1daf4aa6ee201a0d8027b3b
Colin Hayes <colin.hayes985@gmail.com>
2018-07-09 19:13:16 -0400

    Style MessageForm

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -12,7 +12,7 @@ class MessageForm extends Component{
     }
     render(){
         return(
-            <form 
+            /*<form 
              className="MessageForm" 
              onSubmit={this.handleSubmit}
             >
@@ -28,9 +28,57 @@ class MessageForm extends Component{
                 <button type="submit">
                     Send
                 </button>
-            </form>
+            </form>*/
+        <form class="MessageForm" style={styles.MessageForm}>
+            <div class="chatIcon" style={styles.chatIcon}>
+                <i class="fas fa-comment-alt"></i>
+            </div>
+            <input type="text" placeholder="Type a message..." style={styles.input}/>
+            <button type="submit" style={styles.button}>
+                <i class="far fa-paper-plane" title="Send"></i>
+            </button>
+        </form>
         )
     }
+}
+const styles={
+    MessageForm: {
+        backgroundColor: 'white',
+        height: '3rem',
+        display: 'flex',
+        alignItems: 'stretch',
+        border: '2px solid #999',
+        borderRadius: '0.5rem',
+        margin: '0.25rem',
+        padding: '0',
+      },
+      chatIcon: {
+        display: 'flex',
+        borderRadius: '0.5rem',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        color: '#ccc',
+        padding: '0 0.5rem',
+        fontSize: '1.2rem',
+      },
+      input: {
+        flex: '1',
+        fontSize: '1.2rem',
+        border: '0',
+      },
+      //input:focus {
+      //  outline: 0;
+      //}
+      button: {
+        fontSize: '1.5rem',
+        backgroundColor: '#1A8FE3',
+        color: 'white',
+        paddingLeft: '1rem',
+        paddingRight: '1rem',
+        borderTopRightRadius: '0.5rem',
+        borderBottomRightRadius: '0.5rem',
+        border: '1px solid white',
+      }
 }
 
 export default MessageForm
