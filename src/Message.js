b6623a28412b3923646c17460b3e5f099fa4077f
Colin Hayes <colin.hayes985@gmail.com>
2018-07-10 00:03:29 -0400

    Style MessageList and prepare styles for Message

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const Message = (props) => {
     return(
-        <div className="Message">
+        <div className="Message" style={styles.Message}>
             {props.message.user.displayName}:{props.message.body}
         </div>
     )
+}
+
+const styles={
+    Message:{
+        display:'flex',
+        marginTop:'1rem',
+        padding:'0 1rem',
+    },
+    Details:{
+        flex: '1',
+        paddingLeft:'.5rem',
+    },
 }
 
 export default Message;
