95445aedad561094c34824bfcd618d3f2041f5ee
Colin Hayes <colin.hayes985@gmail.com>
2018-07-09 23:48:50 -0400

    Style SignOut component

--- src/SignOut.js
+++ src/SignOut.js
@@ -4,9 +4,25 @@ import {StyleSheet, css} from 'aphrodite'
 const SignOut = () => {
     return(
         <button>
-+           <i className="fas fa-sign-out-alt"></i>
-+       </button>
+           <i className="fas fa-sign-out-alt"></i>
+       </button>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        border: 0,
+        padding: 0,
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.6)',
+        fontSize: '1.2rem',
+        transition: 'color 0.25s ease-out',
+        cursor: 'pointer',
+        ':hover': {
+            color: 'white',
+        },
+      }
+    }
+)
 
 export default SignOut;
